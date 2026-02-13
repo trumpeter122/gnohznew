db49a363c7680be5e16329cfdcc94cfefb4ef265
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2026-02-13 13:02:39 +0800

    chore: remove github workflows; fix mock; fix base name

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import {Suspense} from 'react'
-import {ErrorBoundary, type FallbackProps} from 'react-error-boundary'
-import {Route, Routes} from 'react-router'
-import {LoadingOrError} from '@/components/LoadingOrError'
+import { Suspense } from 'react'
+import { ErrorBoundary, type FallbackProps } from 'react-error-boundary'
+import { Route, Routes } from 'react-router'
+import { LoadingOrError } from '@/components/LoadingOrError'
 import Translation from '@/pages/Translation'
 
-function renderError({error}: FallbackProps) {
-	return <LoadingOrError error={error} />
+function renderError({ error }: FallbackProps) {
+  return <LoadingOrError error={error} />
 }
 
 export function App() {
-	return (
-		<ErrorBoundary fallbackRender={renderError}>
-			<Suspense fallback={<LoadingOrError />}>
-				<Routes>
-					<Route element={<Translation />} path='' />
-				</Routes>
-			</Suspense>
-		</ErrorBoundary>
-	)
+  return (
+    <ErrorBoundary fallbackRender={renderError}>
+      <Suspense fallback={<LoadingOrError />}>
+        <Routes>
+          <Route element={<Translation />} path='gnohznew' />
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
